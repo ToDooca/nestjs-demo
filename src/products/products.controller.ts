5178faa324de17082399d45a1f459881745cbead
Dusan Stankovic <todooc4@gmail.com>
2022-11-25 16:59:44 +0100

    created getAllProducts and getProduct endpoints

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Post, Body } from "@nestjs/common";
+import { Controller, Post, Body, Get, Param } from "@nestjs/common";
 import { ProductsService } from "./products.service";
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-
   @Post()
-  addProduct(@Body() productBody: { title: string, description: string, price: number }): any {
+  addProduct(@Body() productBody: { title: string, description: string, price: number }): { id: string } {
     const generatedId = this.productsService.insertProduct(productBody.title, productBody.description, productBody.price);
     return { id: generatedId };
+  }
+
+  @Get()
+  getAllProducts() {
+    return this.productsService.getAllProducts();
+  }
+  @Get(':id')
+  getProduct(@Param('id') prodId: string) {
+    return this.productsService.getProduct(prodId);
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,16 +1,28 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { Product } from "./product.entity";
 
 @Injectable()
 export class ProductsService {
-  products: Product[] = [];
+  private products: Product[] = [];
 
   insertProduct(title: string, desc: string, price: number) {
-    // prodId is a current timestamp just for demo purposes,
+    // prodId is random number just for demo purposes,
     // I would not use this in production!!!
-    const prodId = new Date().toString();
+    const prodId = Math.random().toString();
     const newProduct = new Product(prodId, title, desc, price);
     this.products.push(newProduct);
     return prodId;
+  }
+
+  getAllProducts() {
+    return [...this.products];
+  }
+
+  getProduct(prodId: string) {
+    const product = this.products.find(prod => prod.id === prodId)
+    if (!product) {
+      throw new NotFoundException('Could not find product.');
+    }
+    return {...product};
   }
 }
